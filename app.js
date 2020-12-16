c54cf5fa64f9fa9685db250b27f91eaf498e0f27
Bret Comnes <bcomnes@gmail.com>
2020-12-16 08:50:43 -0700

    bootstrap fastify

---
+++ app.js
@@ -0,0 +1,23 @@
+import path from 'path'
+import AutoLoad from 'fastify-autoload'
+
+export async function app (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
--- index.test.js
+++
@@ -1,7 +0,0 @@
-import tap from 'tap'
-import { foo } from './index.js'
-
-tap.test('exports', async (t) => {
-  t.ok(foo)
-  t.equal(foo.bar, 'baz', 'exports as expected')
-})
