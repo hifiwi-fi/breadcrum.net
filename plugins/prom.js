dcce7842be64535981ca2b9a8295efadb3b059c7
Bret Comnes <bcomnes@gmail.com>
2022-04-01 21:30:13 -0700

    Add Prometheus endpoint

---
+++ plugins/prom.js
@@ -0,0 +1,42 @@
+import fp from 'fastify-plugin'
+import Fastify from 'fastify'
+
+/**
+ * This plugins adds promethius metrics
+ *
+ * @see https://gitlab.com/m03geek/fastify-metrics
+ */
+export default fp(async function (fastify, opts) {
+  fastify.register(import('fastify-metrics'), {})
+
+  const promServer = Fastify({
+    logger: true
+  })
+
+  promServer.route({
+    url: '/metrics',
+    method: 'GET',
+    logLevel: 'info',
+    schema: {
+      // hide route from swagger plugins
+      hide: true
+    },
+    handler: async (_, reply) => {
+      reply.type('text/plain').send(await fastify.metrics.client.register.metrics())
+    }
+  })
+
+  const start = async () => {
+    try {
+      await promServer.listen(9091, '0.0.0.0')
+    } catch (err) {
+      promServer.log.error(err)
+      process.exit(1)
+    }
+  }
+  await start()
+},
+{
+  name: 'prom',
+  dependencies: ['env']
+})
