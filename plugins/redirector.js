91c0d91498d2e570bf732ddc63a845f6191b7703
Bret Comnes <bcomnes@gmail.com>
2022-04-02 16:57:02 -0700

    Debug

--- plugins/redirector.js
+++ plugins/redirector.js
@@ -2,9 +2,10 @@ import fp from 'fastify-plugin'
 
 export default fp(async function (fastify, opts) {
   fastify.addHook('onRequest', async (request, reply) => {
-    if (fastify.config.ENV === 'production' && request.headers.host !== fastify.config.DOMAIN) {
-      return reply.redirect(301, 'https://' + fastify.config.DOMAIN + request.url)
-    }
+    request.log.info({ host: request.headers.host, domain: fastify.config.DOMAIN, url: request.url })
+    // if (fastify.config.ENV === 'production' && request.headers.host !== fastify.config.DOMAIN) {
+    //  return reply.redirect(301, 'https://' + fastify.config.DOMAIN + request.url)
+    // }
   })
 }, {
   name: 'redirector',
