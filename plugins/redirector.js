ab79ba48ba45a7ab8011a659b286c1b841f600b6
Bret Comnes <bcomnes@gmail.com>
2022-04-02 17:06:43 -0700

    Fix typo

--- plugins/redirector.js
+++ plugins/redirector.js
@@ -2,10 +2,9 @@ import fp from 'fastify-plugin'
 
 export default fp(async function (fastify, opts) {
   fastify.addHook('onRequest', async (request, reply) => {
-    request.log.info({ host: request.headers.host, domain: fastify.config.DOMAIN, url: request.url })
-    // if (fastify.config.ENV === 'production' && request.headers.host !== fastify.config.DOMAIN) {
-    //  return reply.redirect(301, 'https://' + fastify.config.DOMAIN + request.url)
-    // }
+    if (fastify.config.ENV === 'production' && request.headers.host !== fastify.config.DOMAIN) {
+      return reply.redirect(301, 'https://' + fastify.config.DOMAIN + request.url)
+    }
   })
 }, {
   name: 'redirector',
