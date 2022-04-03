676bc43b4b17c09cb31c7682afd2092f1994ee0b
Bret Comnes <bcomnes@gmail.com>
2022-04-02 18:38:55 -0700

    Fix redirector this time for real

--- plugins/redirector.js
+++ plugins/redirector.js
@@ -2,7 +2,10 @@ import fp from 'fastify-plugin'
 
 export default fp(async function (fastify, opts) {
   fastify.addHook('onRequest', async (request, reply) => {
-    if (fastify.config.ENV === 'production' && request.headers.host !== fastify.config.DOMAIN) {
+    if (
+      fastify.config.ENV === 'production' &&
+      (request.headers?.host?.startsWith('www.') || request.headers?.host?.endsWith('.fly.dev'))
+    ) {
       return reply.redirect(301, 'https://' + fastify.config.DOMAIN + request.url)
     }
   })
