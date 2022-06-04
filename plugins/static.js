ead868bca257ca5187cba7d3759c1e2ecbb22c1a
Bret Comnes <bcomnes@gmail.com>
2022-06-04 14:04:39 -0600

    Try removing broken date based cache

--- plugins/static.js
+++ plugins/static.js
@@ -13,7 +13,8 @@ export default fp(async function (fastify, opts) {
   fastify.register(import('@fastify/static'), {
     root: path.join(__dirname, '../public'),
     prefix: '/',
-    redirect: true
+    redirect: true,
+    lastModified: false // Always showing  Tue, 01 Jan 1980 00:00:01 GMT in prod for some reason
   })
 }, {
   name: 'static',
