fe3ab4bff8659bb005bd195a1f6df695d07f176e
Bret Comnes <bcomnes@gmail.com>
2022-07-04 19:32:36 -0600

    Only start prom server in prod

--- plugins/prom.js
+++ plugins/prom.js
@@ -47,7 +47,7 @@ export default fp(async function (fastify, opts) {
   fastify.addHook('onClose', async (instance) => {
     await promServer.close()
   })
-  await start()
+  if (fastify.config.ENV === 'production') await start()
 },
 {
   name: 'prom',
