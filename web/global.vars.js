8f5693c87a99a0997136eaf194727b3cdb87e41e
Bret Comnes <bcomnes@gmail.com>
2022-07-18 19:10:40 -0600

    Disable registration on build as well
    Fix deploy transport
    
    Fix test env
    
    Only use dontenv in the frontend
    
    Fix it again

--- web/global.vars.js
+++ web/global.vars.js
@@ -1,24 +1,18 @@
-import envSchema from 'env-schema'
+import 'dotenv/config'
 import { readFile } from 'fs/promises'
 import desm from 'desm'
 import { join } from 'path'
-import { schema } from '../plugins/env.js'
-
-const config = envSchema({
-  schema,
-  dotenv: true
-})
 
 const __dirname = desm(import.meta.url)
 
 export default async () => {
   return {
     siteName: '🥖 Breadcrum',
-    registration: config.REGISTRATION,
+    registration: process.env.REGISTRATION,
     version: JSON.parse(await readFile(new URL(join(__dirname, '../package.json'), import.meta.url))).version
   }
 }
 
 export const browser = {
-  'process.env.REGISTRATION': config.REGISTRATION
+  'process.env.REGISTRATION': process.env.REGISTRATION
 }
