0aad494e02efeb68e201cb1d338e28f35e4bc351
Bret Comnes <bcomnes@gmail.com>
2022-07-18 19:02:10 -0600

    Add basic default feed route

--- routes/api/feeds/index.js
+++ routes/api/feeds/index.js
@@ -3,8 +3,70 @@ import jsonfeedToRSS from 'jsonfeed-to-rss'
 import cleanDeep from 'clean-deep'
 import { getYTDLPUrl } from '../../../lib/run-yt-dlp.js'
 import { cache } from '../../../lib/temp-cache.js'
+import { getOrCreateDefaultFeed } from '../../../lib/get-or-create-default-feed.js'
 
 export default async function podcastFeedsRoutes (fastify, opts) {
+  fastify.get(
+    '/default-feed',
+    {
+      preHandler: fastify.auth([fastify.verifyJWT]),
+      schema: {
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              data: {
+                type: 'object',
+                properties: {
+                  url: {
+                    type: 'string',
+                    format: 'uri'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    async function getDefaultFeedHandler (request, reply) {
+      const userId = request.user.id
+
+      const feedId = await getOrCreateDefaultFeed({ userId, client: fastify.pg })
+
+      const feedQuery = SQL`
+          select
+            pf.id,
+            pf.created_at,
+            pf.updated_at,
+            pf.title,
+            pf.description,
+            pf.image_url,
+            pf.explicit,
+            pf.token,
+            u.username as owner_name
+          from podcast_feeds pf
+          join users u
+          on pf.owner_id = u.id
+          where pf.id = ${feedId}
+          and pf.owner_id = ${userId}
+          fetch first row only;
+        `
+
+      const feedResults = await fastify.pg.query(feedQuery)
+
+      const pf = feedResults.rows.pop()
+      if (!pf) {
+        return reply.notFound(`podcast feed ${feedId} not found`)
+      }
+
+      return {
+        data: {
+          url: `${fastify.config.TRANSPORT}://${userId}:${pf.token}@${fastify.config.HOST}/api/feeds/${pf.id}`
+        }
+      }
+    }
+  )
   fastify.get(
     '/:feed',
     {
