6fb172049bfd5f3740008b1d78726c3769630341
Bret Comnes <bcomnes@gmail.com>
2022-07-28 21:10:33 -0600

    Insurment some prom metrics

--- plugins/queue.js
+++ plugins/queue.js
@@ -8,24 +8,42 @@ export default fp(async function (fastify, opts) {
   })
 
   let count = 0
+  const qCounter = new fastify.metrics.client.Counter({
+    name: 'queue_count',
+    help: 'The number of times the queue has been added to'
+  })
+  const qErrorCounter = new fastify.metrics.client.Counter({
+    name: 'queue_error_count',
+    help: 'The number of times the queue has been added to'
+  })
+  const qGauge = new fastify.metrics.client.Gauge({ name: 'queue_size', help: 'The size of the queue' })
+  const pendingQGauge = new fastify.metrics.client.Gauge({ name: 'queue_pending_size', help: 'The size of the pending queue' })
 
   queue.on('active', () => {
+    qCounter.inc()
     fastify.log.info(`Working on item #${++count}.  Size: ${queue.size}  Pending: ${queue.pending}`)
   })
 
   queue.on('error', error => {
+    qErrorCounter.inc()
     fastify.log.error(error)
   })
 
   queue.on('idle', () => {
+    qGauge.set(queue.size)
+    pendingQGauge.set(queue.pending)
     fastify.log.info(`Queue is idle.  Size: ${queue.size}  Pending: ${queue.pending}`)
   })
 
   queue.on('add', () => {
+    qGauge.set(queue.size)
+    pendingQGauge.set(queue.pending)
     fastify.log.info(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`)
   })
 
   queue.on('next', () => {
+    qGauge.set(queue.size)
+    pendingQGauge.set(queue.pending)
     fastify.log.info(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`)
   })
 
@@ -33,5 +51,5 @@ export default fp(async function (fastify, opts) {
 },
 {
   name: 'queue',
-  dependencies: ['env']
+  dependencies: ['env', 'prom']
 })
