26063319c58eaf1260cbc119da41fb021b9dae08
Bret Comnes <bcomnes@gmail.com>
2022-08-09 17:51:13 -0600

    Boostrap yt-dlp in testing
    Fixes a race condition that was causing tests to fail. Eventually all of this will get removed.
    
    Closes https://github.com/hifiwi-fi/breadcrum.net/issues/131

---
+++ scripts/bootstrap-yt-dlp.js
@@ -0,0 +1,20 @@
+import YTDlpWrapExports from 'yt-dlp-wrap'
+import cp from 'child_process'
+import path from 'path'
+import util from 'util'
+import desm from 'desm'
+
+const exec = util.promisify(cp.exec)
+
+const __dirname = desm(import.meta.url)
+
+const YTDlpWrap = YTDlpWrapExports.default
+
+try {
+  const { stdout } = await exec('which yt-dlp')
+  console.log(`yt-dlp found: ${stdout.trim()}`)
+} catch (err) {
+  console.warn('Error looking for yt-dlp, downloading it')
+  await YTDlpWrap.downloadFromGithub()
+  console.log(`yt-dlp downloaded: ${path.join(__dirname, '../yt-dlp')}`)
+}
