c536e35277e9dbeb20cc0f25dfb222ee00d3e584
Bret Comnes <bcomnes@gmail.com>
2022-08-19 14:00:29 -0600

    Add swagger route

---
+++ plugins/swagger.js
@@ -0,0 +1,23 @@
+import fp from 'fastify-plugin'
+
+/**
+ * This plugins adds fastify-swagger
+ *
+ * @see https://github.com/fastify/fastify-swagger
+ */
+export default fp(async function (fastify, opts) {
+  fastify.register(import('@fastify/swagger'), {
+    routePrefix: '/documentation',
+    exposeRoute: false,
+    openapi: {
+      info: {
+        title: 'Test swagger',
+        description: 'testing the fastify swagger api',
+        version: '0.1.0'
+      }
+    }
+  })
+}, {
+  name: 'swagger',
+  dependencies: ['env']
+})
