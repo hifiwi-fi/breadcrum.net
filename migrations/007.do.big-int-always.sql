68114e2ab10c52ddbe36bd40c647e164414fbd58
Bret Comnes <bcomnes@gmail.com>
2022-09-21 11:18:47 -0600

    Use bigint

---
+++ migrations/007.do.big-int-always.sql
@@ -0,0 +1,3 @@
+alter table episodes
+  alter column size_in_bytes type bigint,
+  alter column duration_in_seconds type bigint;
---
+++ migrations/007.undo.big-int-always.sql
@@ -0,0 +1,3 @@
+alter table episodes
+  alter column size_in_bytes type integer,
+  alter column duration_in_seconds type integer;
