43ca3d35ea9091492f7aeb3849ae9d75464ea8eb
Bret Comnes <bcomnes@gmail.com>
2022-11-17 09:43:46 -0700

    Handle SNS text/plain with contentTypeParser

--- routes/api/sns/autohooks.js
+++ routes/api/sns/autohooks.js
@@ -1,3 +1,5 @@
+import jsonParser from 'fast-json-body'
+
 export default async function (fastify, opts) {
   // Add basic auth for feed and feed episode routes
   fastify.register(import('@fastify/basic-auth'), {
@@ -11,4 +13,12 @@ export default async function (fastify, opts) {
     if (snsUser !== fastify.config.SNS_USER) throw new Error('Unknown SNS user')
     if (snsPass !== fastify.config.SNS_PASS) throw new Error('Bad password')
   }
+
+  fastify.removeContentTypeParser(['text/plain'])
+
+  fastify.addContentTypeParser('text/plain', function (request, payload, done) {
+    jsonParser(payload, function (err, body) {
+      done(err, body)
+    })
+  })
 }
--- routes/api/sns/index.js
+++ routes/api/sns/index.js
@@ -8,17 +8,24 @@ export default async function snsRoutes (fastify, opts) {
       preHandler: fastify.auth([fastify.basicAuth])
     },
     async function postSNSHandler (request, reply) {
-      const textBody = request.body
-      const data = JSON.parse(textBody)
+      const data = request.body
+
+      if (data.Message) {
+        try {
+          data.Message = JSON.parse(data.Message)
+        } catch (err) {
+          fastify.log.error({ err }, 'Error parsing SNS Notification Message')
+        }
+      }
 
       const insertQuery = SQL`
         insert into sns (body)
-        values (${textBody})
+        values (${JSON.stringify(data)})
       `
 
       await fastify.pg.query(insertQuery)
 
-      fastify.logger.info({ snsData: data })
+      fastify.log.info({ snsData: data })
 
       if (data.Type === 'SubscriptionConfirmation') {
         fastify.log.info(`Confirming subscription: ${data.SubscribeURL}`)
@@ -34,6 +41,24 @@ export default async function snsRoutes (fastify, opts) {
           textBody: await body.text(),
           trailers
         })
+      } else if (data.Type === 'Notification') {
+        switch (data.Message?.notificationType) {
+          case 'Bounce': {
+            fastify.log.info('Bounce')
+            break
+          }
+          case 'Complaint': {
+            fastify.log.info('Complaint')
+            break
+          }
+          case 'Delivery': {
+            fastify.log.info('Delivery')
+            break
+          }
+          default: {
+            fastify.log.info({ notificationType: data.Message?.notificationType }, 'Unknown delivery type')
+          }
+        }
       } else {
         fastify.log.info(`No action: ${data.Type}`)
       }
