0dda61695ee5c4ca4c9af829d5cfdac14d585b7b
Bret Comnes <bcomnes@gmail.com>
2022-12-27 09:16:05 -0700

    Swap yt-dlp-wrap with bc-dlp

--- scripts/bootstrap-yt-dlp.js
+++ scripts/bootstrap-yt-dlp.js
@@ -1,4 +1,4 @@
-import YTDlpWrapExports from 'yt-dlp-wrap'
+import { downloadFromGithub } from 'bc-dlp'
 import cp from 'child_process'
 import path from 'path'
 import util from 'util'
@@ -8,14 +8,12 @@ const exec = util.promisify(cp.exec)
 
 const __dirname = desm(import.meta.url)
 
-const YTDlpWrap = YTDlpWrapExports.default
-
 try {
   const { stdout } = await exec('which yt-dlp')
   console.log(`yt-dlp found: ${stdout.trim()}`)
 } catch (err) {
   console.warn('yt-dlp not found, downloading it')
-  await YTDlpWrap.downloadFromGithub()
+  await downloadFromGithub()
   console.log(`yt-dlp downloaded: ${path.join(__dirname, '../yt-dlp')}`)
   process.exit(0)
 }
