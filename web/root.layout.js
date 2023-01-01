aeedb22397ad4570564b9f98a6f8e2ecb5804fc1
Bret Comnes <bcomnes@gmail.com>
2023-01-01 15:04:27 -0700

    Add app icon

--- web/root.layout.js
+++ web/root.layout.js
@@ -25,6 +25,16 @@ export default function defaultRootLayout ({
       <meta name='viewport' content='width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1'>
       <meta name='referrer' content='no-referrer'>
       <meta name='description' content='Breadcrum internet newspaper clippings and bookmarks. Podcast anything.'>
+
+      <link rel="shortcut icon" href="/favicon.ico">
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-96x96.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon-180x180.png">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+      <meta name="apple-mobile-web-app-title" content="Breadcrum">
+
       ${scripts
         ? scripts.map(script => html`<script type='module' src="${script}"></script>`)
         : null}
