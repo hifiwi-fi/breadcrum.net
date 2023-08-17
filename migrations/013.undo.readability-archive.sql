288c5fdf8827213bdf7b1da99f719dfdd45f886b
Bret Comnes <bcomnes@gmail.com>
2023-08-17 16:20:47 -0700

    Fix a broken down migration

--- migrations/013.undo.readability-archive.sql
+++ migrations/013.undo.readability-archive.sql
@@ -1,8 +1,15 @@
-drop trigger if exists set_timestamp_archives ON readabilty_archives;
-drop index if exists idx_archives_bookmark;
-drop index if exists idx_archives_owner;
-drop table if exists archives;
-drop type if exists episode_type;
+-- 1. Drop the new column from the bookmarks table
+ALTER TABLE bookmarks DROP COLUMN summary;
 
-alter table bookmarks
-  drop column if exists summary;
+-- 2. Drop the trigger on the archives table
+DROP TRIGGER IF EXISTS set_timestamp_archives ON archives;
+
+-- 3. Drop the indexes created on the archives table
+DROP INDEX IF EXISTS idx_archives_owner;
+DROP INDEX IF EXISTS idx_archives_bookmark;
+
+-- 4. Drop the archives table
+DROP TABLE IF EXISTS archives;
+
+-- 5. Drop the custom type
+DROP TYPE IF EXISTS archive_extraction_method;
