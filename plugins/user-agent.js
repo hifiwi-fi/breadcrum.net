2ea3a3c3cb669bfcf302f55c3199f9f31e24303f
Bret Comnes <bcomnes@gmail.com>
2023-10-29 13:42:20 -0700

    Parse user agent

---
+++ plugins/user-agent.js
@@ -0,0 +1,13 @@
+import fp from 'fastify-plugin'
+
+/**
+ * This plugins adds fastify-user-agent
+ *
+ * @see https://github.com/Eomm/fastify-user-agent
+ */
+export default fp(async function (fastify, opts) {
+  fastify.register(import('fastify-user-agent'))
+}, {
+  name: 'user-agent',
+  dependencies: []
+})
