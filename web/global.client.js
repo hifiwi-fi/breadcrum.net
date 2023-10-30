8348c61faaf71f99555c1411ca4c0063b91bbd44
Bret Comnes <bcomnes@gmail.com>
2023-10-29 21:42:08 -0700

    Service worker experiment

--- web/global.client.js
+++ web/global.client.js
@@ -1,6 +1,7 @@
 import { toggleTheme } from 'mine.css'
 import 'fragmentions'
 import { render } from 'uland-isomorphic'
+import { loadServiceWorker } from '@siteup/cli/load-service-worker.js'
 import { header } from './components/header/index.js'
 
 window.toggleTheme = toggleTheme
@@ -8,3 +9,5 @@ window.toggleTheme = toggleTheme
 if (typeof window !== 'undefined') {
   render(document.querySelector('.bc-header'), header)
 }
+
+loadServiceWorker()
