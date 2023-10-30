00915b4a35282dd50b7c965d00ef68079be80216
Bret Comnes <bcomnes@gmail.com>
2023-10-29 21:33:08 -0700

    Fix CSS bug

--- web/feeds.template.js
+++ web/feeds.template.js
@@ -1,6 +1,13 @@
 import pMap from 'p-map'
 import jsonfeedToAtom from 'jsonfeed-to-atom'
 
+/**
+ * @typedef {import('@siteup/cli').TemplateFunction} TemplateFunction
+ */
+
+/**
+ * @type {TemplateFunction}
+ */
 export default async function * feedsTemplate (args) {
   const {
     vars: {
@@ -15,7 +22,9 @@ export default async function * feedsTemplate (args) {
     pages
   } = args
   const blogPosts = pages
-    .filter(page => page.vars.layout === 'article')
+    // eslint-disable-next-line dot-notation
+    .filter(page => page.vars['layout'] === 'article')
+    // @ts-ignore
     .sort((a, b) => new Date(b.vars.publishDate) - new Date(a.vars.publishDate))
     .slice(0, 10)
 
@@ -36,8 +45,8 @@ export default async function * feedsTemplate (args) {
       return {
         date_published: page.vars.publishDate,
         title: page.vars.title,
-        url: `${baseUrl}/${page.page.path}/`,
-        id: `${baseUrl}/${page.page.path}/#${page.vars.publishDate}`,
+        url: `${baseUrl}/${page.pageInfo.path}/`,
+        id: `${baseUrl}/${page.pageInfo.path}/#${page.vars.publishDate}`,
         content_html: await page.renderInnerPage({ pages })
       }
     }, { concurrency: 4 })
