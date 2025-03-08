92dae28c8e07105a891441d62c983af9b0fe4132
Bret Comnes <bcomnes@gmail.com>
2025-03-08 14:09:26 -0800

    Experiment with typestripping

--- packages/web/plugins/auth-decorators.d.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyRequest } from 'fastify'
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    verifyAdmin(request: FastifyRequest): Promise<void>;
-    notDisabled(request: FastifyRequest): Promise<void>;
-  }
-}
