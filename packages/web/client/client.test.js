d4c2f6b12837311b178956f6ec07753b3308e01c
Bret Comnes <bcomnes@gmail.com>
2025-05-24 09:57:40 -0700

    Organize tests

--- packages/web/client/client.test.js
+++ packages/web/client/client.test.js
@@ -1,12 +1,14 @@
-import { test } from 'node:test'
+import { test, suite } from 'node:test'
 import assert from 'node:assert'
 import { page } from './client.js'
 import { render } from 'uland-isomorphic'
 
-test('Testing is set up and working', async (t) => {
-  let rendered
-  assert.doesNotThrow(() => {
-    rendered = render(String, page)
+suite('Main client page tests', () => {
+  test('Main page component renders without errors', async (t) => {
+    let rendered
+    assert.doesNotThrow(() => {
+      rendered = render(String, page)
+    }, 'page renders without throwing exceptions')
+    assert.strictEqual(typeof rendered, 'string', 'page renders to a string value')
   })
-  assert.strictEqual(typeof rendered, 'string')
 })
--- packages/web/client/search/bookmarks/client.test.js
+++ packages/web/client/search/bookmarks/client.test.js
@@ -1,12 +1,14 @@
-import { test } from 'node:test'
+import { test, suite } from 'node:test'
 import assert from 'node:assert'
 import { page } from './client.js'
 import { render } from 'uland-isomorphic'
 
-test('Testing is set up and working', async () => {
-  let rendered
-  assert.doesNotThrow(() => {
-    rendered = render(String, page)
-  }, 'page renders without error')
-  assert.strictEqual(typeof rendered, 'string', 'page renders to string')
+suite('Bookmarks Search Page Tests', () => {
+  test('Bookmarks search page component renders without errors', async () => {
+    let rendered
+    assert.doesNotThrow(() => {
+      rendered = render(String, page)
+    }, 'page renders without error')
+    assert.strictEqual(typeof rendered, 'string', 'page renders to string')
+  })
 })
